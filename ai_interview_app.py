900242c1efd198d8806a25a9a2199f9729fa6796
Banoth Rajesham <dropoutworld154@gmail.com>
2025-09-04 19:18:34 +0530

    Update ai_interview_app.py

--- ai_interview_app.py
+++ ai_interview_app.py
@@ -226,8 +226,8 @@ def interview_section():
         if "audio_buffer" not in st.session_state: st.session_state.audio_buffer = []
         if "proctoring_img" not in st.session_state: st.session_state.proctoring_img = None
         
-        webrtc_ctx = webrtc_streamer(key=f"interview_cam_{idx}", mode=WebRtcMode.SENDRECV, rtc_configuration=RTC_CONFIGURATION, media_stream_constraints={"video": True, "audio": True}, processor_factory=InterviewProcessor, async_processing=True)
-        
+       webrtc_ctx = webrtc_streamer(key=f"interview_cam_{idx}", mode=WebRtcMode.SENDRECV, rtc_configuration=RTC_CONFIGURATION, media_stream_constraints={"video": True, "audio": True}, video_processor_factory=InterviewProcessor, async_processing=True)
+
         if webrtc_ctx.state.playing and webrtc_ctx.processor:
             st.session_state.audio_buffer.extend(webrtc_ctx.processor.audio_buffer)
             webrtc_ctx.processor.audio_buffer.clear()
